dfe6c2dd9dc41b1bb81e736026b7d1adebc92069
UzziKyle <uzzielkyleynciong0824@gmail.com>
2023-11-05 20:05:05 +0800

    update logic functions

--- utils/pdf_logics.py
+++ utils/pdf_logics.py
@@ -1,16 +1,52 @@
-from PyPDF2 import PdfMerger
+# Needs to clean this up
+from PyPDF2 import PdfMerger, PdfFileReader
 import os
+import glob
 
+
+def get_pdf_page(file_path: str) -> int:
+    """Returns the number of pages of a given PDF file
+
+    Args:
+        file_path (str): path to file
+
+    Returns:
+        int: total number of pages
+    """
+    file = open(file_path, 'rb')
+    
+    # store data in pdfReader 
+    pdf_reader = PdfFileReader(file) 
+    
+    # count number of pages 
+    total_pages = len(pdf_reader.pages) 
+    
+    return  total_pages
+
+
+def get_pdfs(root_dir: list) -> list:
+    files = []
+    for row_num, folder in enumerate(root_dir):
+        str_path = os.path.join(folder, '*.pdf') # Use os.path.join for paths
+        pdfs = [path.replace("\\","/") for path in glob.glob(str_path, recursive=True) if os.path.isfile(path)] # also check if the glob is a pdf file, it may sometimes be a folder that ends with .pdf
+        # .replace("\\","/") for 'slash' consistency... https://stackoverflow.com/a/18776536, tnx
+        files.append(pdfs)
+        
 
 #  Credits to Ma'am Tine (https://github.com/kjvmartinez) for this function
 def merge_pdf(pdfs: list[str], filename: str):
-    merger = PdfMerger()
-    
-    for pdf_file in pdfs:
-        merger.append(pdf_file)
-        
-    merger.write(filename)
-    merger.close()
+        if len(pdfs) == 1:  # If there's only one PDF file, skip merging
+            return 0
+
+        merger = PdfMerger()
+        for pdf_file in pdfs:
+            merger.append(pdf_file)
+            
+            # merger.append(i)
+        merger.write(filename)
+        num_pages = len(merger.pages)
+        merger.close()
+        return num_pages
     
 
 # Credits to jbarry302 (https://github.com/jbarry302) for all the functions below
@@ -75,3 +111,49 @@ def has_at_least_two_pdfs(folder_path: str) -> bool:
                 if count >= 2:
                     return True
     return False
+
+
+def merge_files(main_path: str, root_dir: list) -> None:
+    files = []
+
+    for row_num, folder in enumerate(root_dir):
+        str_path = os.path.join(folder, '*.pdf') # Use os.path.join for paths
+        pdfs = [path.replace("\\","/") for path in glob.glob(str_path, recursive=True) if os.path.isfile(path)] # also check if the glob is a pdf file, it may sometimes be a folder that ends with .pdf
+        # .replace("\\","/") for 'slash' consistency... https://stackoverflow.com/a/18776536, tnx
+        files = files + pdfs
+        number_of_pages = get_num_pages(pdfs)
+
+    files = flatten_array(files)
+    total_pages = get_num_pages(files)
+    # by default, save the output to the first folder
+    output_path = os.path.join(root_dir[0], 'output.pdf')
+    merge_pdf(files, output_path)
+    
+
+def scan_folders(folder_path: str) -> list:
+    list_roots = []
+
+    for (root, dirs, files) in os.walk(folder_path, topdown=True):
+        if any(f.endswith('.pdf') for f in files):
+            list_roots.append(root)
+
+    return list_roots
+
+
+# This ugly function is made by me
+def recursive_merging(main_path: str, dirs: list) -> tuple[list]:
+    folders = []
+    outputs = []
+    
+    for folder in dirs:
+        str_path = os.path.join(folder, '*.pdf') # Use os.path.join for paths
+        pdfs = [path.replace("\\","/") for path in glob.glob(str_path, recursive=True) if os.path.isfile(path) and not os.path.basename(path).startswith('output')]        
+        
+        number_of_pages = merge_pdf(pdfs, os.path.join(folder, 'output.pdf'))
+    
+        if number_of_pages == 0: continue
+        
+        folders.append(folder.replace(main_path, './'))
+        outputs.append(('output.pdf', number_of_pages))
+        
+    return folders, outputs
