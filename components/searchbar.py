56cf7a4b8d67c357fb5b55d2f332034aab21d411
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-11-09 05:14:18 +0800

    first full version release (#2)
    * create searchbar component
    
    * add load folder path functionality
    
    * made minor changes
    
    * create appearance mode toggler component
    
    * add utils package
    
    * add/update components
    
    * update main.py
    
    * add treeview component
    
    * update components
    
    * update logic functions
    
    * update main.py
    
    * add app screenshot
    
    * update requirements.txt
    
    * first version push

---
+++ components/searchbar.py
@@ -0,0 +1,70 @@
+from typing import Optional, Tuple, Union
+from customtkinter import *
+
+
+class Searchbar(CTkFrame):
+    WIDTH: int = 600
+    HEIGHT: int = 50
+    
+    def __init__(self, *args, 
+                 label: str = 'Search Folder:',
+                 placeholder_text: str = 'Folder Path',
+                 bg_color: str | Tuple[str, str] = 'transparent', 
+                 fg_color: str | Tuple[str, str] = 'transparent', 
+                 corner_radius: int = 0,
+                 **kwargs):
+        super().__init__(*args, bg_color=bg_color, fg_color=fg_color, corner_radius=corner_radius, **kwargs)
+              
+        self.grid_columnconfigure((0, 2), weight=0)
+        self.grid_columnconfigure(1, weight=1)
+        
+        self.lbl_search = CTkLabel(self, text=label, width=90)
+        self.lbl_search.grid(row=0, column=0, padx=0, pady=(10,5))
+        
+        self.entry = CTkEntry(self, placeholder_text=placeholder_text, width=self.WIDTH - 210)
+        self.entry.grid(row=0, column=1, padx=10, pady=(10,5), sticky='ew')
+        self.entry.bind('<Return>', self.remove_entry_focus)
+        
+        self.load_btn = CTkButton(self, text="Load", width=90, command=self.load, corner_radius=corner_radius)
+        self.load_btn.grid(row=0, column=2, padx=(10,0) , pady=(10,5))
+        
+    def remove_entry_focus(self, event):
+        self.focus()
+        
+    def load(self):
+        folder_path = filedialog.askdirectory()
+        
+        if folder_path:
+            self.clear()
+            self.set(folder_path=folder_path)
+                    
+    def get(self) -> str:
+        return self.entry.get()
+    
+    def set(self, folder_path: str):
+        self.clear()
+        self.entry.insert(0, folder_path)
+        
+    def clear(self):
+        self.entry.delete(0, 'end')
+        self.entry.configure(placeholder_text='Folder Path')
+        
+
+if __name__ == '__main__':
+    class Tester(CTk):
+        def __init__(self, fg_color: str | Tuple[str, str] | None = None, **kwargs):
+            super().__init__(fg_color, **kwargs)
+            
+            self.grid_columnconfigure(0, weight=1)
+        
+            self.head_frame = CTkFrame(self, fg_color='whitesmoke', corner_radius=0)
+            self.head_frame.grid(row=0, column=0)
+            
+            self.searchbar = Searchbar(master=self.head_frame, width=700)
+            self.searchbar.grid(row=0, column=0, padx=10, pady=(10, 0))
+                    
+            
+    app = Tester()
+
+    app.mainloop()   
+     
--- main.py
+++ main.py
@@ -1,182 +1,31 @@
-'''
-Disclaimer: 
-The way I wrote this code was to somehow make it 'work'... 
-I had no prior experience nor knowledge in app/software development. 
-Constructive feedbacks are very much appreciated :)
-'''
-from PyPDF2 import PdfMerger
-import glob
-import os
-from PyPDF2 import PdfMerger
+from typing import Optional, Tuple, Union
 from customtkinter import *
-
-set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-set_default_color_theme("dark-blue")
-
-EDGY = {'corner_radius': 0}  # To remove round corners
-
-
-'''
-    FUNCTIONS   
-'''
-def toggle_appearance() -> None:
-    current = get_appearance_mode()
-
-    if current == "Light":
-        set_appearance_mode("Dark")
-        return
-    
-    set_appearance_mode("Light")
-
-
-def merge_pdf(files: list[str], filename: str) -> int:
-    if len(files) == 1:  # If there's only one PDF file, skip merging
-        return 0
-
-    merger = PdfMerger()
-    for pdf_file in files:
-        merger.append(pdf_file)
+from components import HeadComponent 
+from components import TreeView
         
-    merger.write(filename)
-    num_pages = len(merger.pages)
-    merger.close()
-    return num_pages
-
-
-def display_no_path(master) -> None:
-    lbl_no_path = CTkLabel(master=master, text='No path provided.')
-    lbl_no_path.grid(row=0, column=0, padx=5, pady=(5, 0), sticky='w')
-
-
-def display_path_pages(master, folder: str, total_pages: int, count: int) -> None:
-    lbl_path = CTkLabel(master=master, text=f"📁 {folder}")
-    lbl_pages = CTkLabel(master=master, text=f"=> {total_pages} pages")
-
-    lbl_path.grid(row=count, column=0, padx=(5, 15), pady=(5, 0), sticky='w')
-    lbl_pages.grid(row=count, column=1, sticky='w')
-    
-
-def display_no_files(master, count: int) -> None:
-    lbl_no_files = CTkLabel(master=master, text='No files to merge...')
-    lbl_no_files.grid(row=count, column=0, padx=5,  pady=(5, 0), sticky='w')
-    
-
-'''
-CLASSES
-'''    
 class App(CTk):
-    def __init__(self) -> None:
-        super().__init__()
-        self.title("PDFMergeXpress")
-        self.geometry("900x420")
-        self.resizable(False, False)
-
-
-class MainFrame(CTkFrame):
-    def __init__(self, container) -> None:
-        super().__init__(container, **EDGY, fg_color='transparent')
-        self.folder_path = ''
-        self.pack()
-
-        frm_search_folder = CTkFrame(self, **EDGY)
-        frm_search_folder.pack(padx=10, pady=(30, 10))
-
-        lbl_folder_path = CTkLabel(frm_search_folder, text="Folder Path", font=CTkFont(size=15, family="monospace"))
-        lbl_folder_path.grid(row=0, column=0, padx=(10, 20))
-
-        btn_toggle_appearance = CTkButton(frm_search_folder, text="Dark/Light", font=CTkFont(size=15, family="monospace"), width=80, **EDGY, command=lambda: toggle_appearance())
-        btn_toggle_appearance.grid(row=1, column=0, padx=(10, 0), sticky='nw')
-
-        ent_folder_path = CTkEntry(frm_search_folder, placeholder_text='Enter folder path or load folder', font=CTkFont(size=13, family="monospace"), width=480, **EDGY)
-        ent_folder_path.grid(row=0, column=1, padx=(0, 20))
-
-        btn_merge = CTkButton(frm_search_folder, text="Merge", font=CTkFont(size=15, family="monospace"), width=120, **EDGY, command=lambda: self.start_merge(scl_display))
-        btn_merge.grid(row=1, column=1, sticky='n')
-
-        btn_update = CTkButton(frm_search_folder, text="Update", font=CTkFont(size=15, family="monospace"), width=150, **EDGY, command=lambda: self.update_folder(ent_folder_path))
-        btn_update.grid(row=0, column=2, padx=(0, 10), pady=(5, 5), sticky='e')
-
-        btn_load_folder = CTkButton(frm_search_folder, text="Load", font=CTkFont(size=15, family="monospace"), width=150, **EDGY, command=lambda: self.load_folder(ent_folder_path))
-        btn_load_folder.grid(row=1, column=2, padx=(0, 10), pady=(0, 5), sticky='e')
-
-        frm_display_folders = CTkFrame(self, **EDGY, width=700)
-        frm_display_folders.pack(pady=(0, 20))
-
-        scl_display = CTkScrollableFrame(frm_display_folders, width=800, height=250, **EDGY)
-        scl_display.pack()
-
-        btn_clear = CTkButton(frm_display_folders, text="Clear All", font=CTkFont(size=15, family="monospace"), **EDGY, command=lambda: self.clear_display(scl_display))
-        btn_clear.pack(fill="x")
-
-    def update_folder(self, ent_folder_path) -> None:
-        self.folder_path = ent_folder_path.get()
-    
-    def load_folder(self, ent_folder_path) -> None:
-        self.folder_path = filedialog.askdirectory()
-
-        if not self.folder_path:
-            return
-
-        if ent_folder_path:
-            ent_folder_path.delete(1, END)
+    def __init__(self, fg_color: str | Tuple[str, str] | None = None, **kwargs):
+        super().__init__(fg_color, **kwargs)
         
-        ent_folder_path.insert(END, self.folder_path)
-
-    @staticmethod
-    def clear_display(scl_display) -> None:
-        for element in scl_display.winfo_children():
-            element.destroy()
-
-    def start_merge(self, scl_display) -> None:
-        self.clear_display(scl_display)
-
-        if not self.folder_path: 
-            display_no_path(master=scl_display)
-            return
-
-        if not self.folder_path.endswith(os.path.sep):
-            self.folder_path += os.path.sep
-            
-        roots_not_empty = self.scan_folders()
-
-        if roots_not_empty: 
-            self.merge_files(root_dir=roots_not_empty, scroll_frame=scl_display)                
-                         
-    @staticmethod
-    def merge_files(root_dir: list, scroll_frame) -> None:
-        count = 0  # placing items in grid rows
-
-        for folder in root_dir:
-            str_path = os.path.join(folder, '*.pdf')  # Use os.path.join for paths
-
-            files = glob.glob(str_path, recursive=True)
-
-            number_of_pages = merge_pdf(files, os.path.join(folder, 'output.pdf'))
-
-            if number_of_pages > 0:
-                display_path_pages(master=scroll_frame, folder=folder, total_pages=number_of_pages, count=count)
-                
-                count += 1
-
-        if count == 0:
-            display_no_files(master=scroll_frame, count=count)    
-                    
-    def scan_folders(self):
-        list_roots = []
+        self.title('PDFMergeXpress')
+        self.geometry('700x310')
+        self.minsize(625, 300)
+        set_appearance_mode('Light')
+        # set_default_color_theme('assets/themes/metal.json')
         
-        for (root, dirs, files) in os.walk(self.folder_path, topdown=True):
-            if any(f.endswith('.pdf') for f in files):
-                list_roots.append(root)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+    
+        self.head = HeadComponent(self)
+        self.head.grid(row=0, column=1, pady=0, sticky='ew')
         
-        return list_roots
-    
-    
-def main() -> None:
+        self.treeview = TreeView(self)
+        self.treeview.grid(row=1, column=1, pady=0, sticky='nsew')
+               
+        self.head.merger.set_treeview_connection(self.treeview.clear, 
+                                                 self.treeview.display_outputs)
+        
+       
+if __name__ == '__main__':
     app = App()
-    main = MainFrame(app)
     app.mainloop()
-
-
-if __name__ == "__main__":
-    main()
-    
